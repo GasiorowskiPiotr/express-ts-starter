4086fe1913b72fbc2220f8e34a0767dcaac1f501
Piotr Gąsiorowski <gasiorowski.piotr@gmail.com>
2016-07-01 22:50:11 +0200

    Initial commit
    Refactored JS to TS from express-generator. Added build pipeline.

---
+++ src/server.ts
@@ -0,0 +1,62 @@
+/// <reference path="typings/index.d.ts" />
+
+import * as express from 'express';
+import * as path from 'path';
+import * as favicon from 'serve-favicon';
+import * as logger from 'morgan';
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
+
+import index from './routes/index';
+import users from './routes/users';
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  (<any>err).status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err: Error, req: express.Request, res: express.Response, next: any) => {
+    res.status((<any>err).status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err: Error, req: express.Request, res: express.Response, next: any) => {
+  res.status((<any>err).status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+export default app;
